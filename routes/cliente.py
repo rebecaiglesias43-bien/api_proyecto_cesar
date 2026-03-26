a6694de6e0bebe2c55f45032a5d790a7832bf5a3
rebecaiglesias43-bien <rebecaiglesias43@gmail.com>
2026-03-26 15:33:30 -0500

    Refactor cliente.py to use Blueprint correctly

--- routes/cliente.py
+++ routes/cliente.py
@@ -1,9 +1,8 @@
-from flask import blueprints
-from controllers.cliente_controllers import  cntlistado
+from flask import Blueprint
+from controllers.cliente_controllers import cntlistado
 
+cliente_bp = Blueprint('cliente', __name__)
 
-cliente_bp = blueprints('cliente', __name__)
-
-@cliente_bp.router('/', methods=["get"])
+@cliente_bp.route('/', methods=["GET"])
 def listado():
     return cntlistado()
