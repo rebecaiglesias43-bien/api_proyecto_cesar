30b40745d31ab659df0026ba6147e17f0afca53b
Jesus Aleman <ospajesu@gmail.com>
2026-03-27 07:56:21 -0500

    Add HorarioModel class with initialization and to_dict method

--- models/horario_model.py
+++ models/horario_model.py
@@ -1,1 +1,16 @@
+class HorarioModel:
+    def __init__(self, id_horario, id_empleado, dia_semana, hora_inicio, hora_fin):
+        self.id_horario = id_horario
+        self.id_empleado = id_empleado
+        self.dia_semana = dia_semana
+        self.hora_inicio = hora_inicio
+        self.hora_fin = hora_fin
 
+    def to_dict(self):
+        return {
+            'id_horario': self.id_horario,
+            'id_empleado': self.id_empleado,
+            'dia_semana': self.dia_semana,
+            'hora_inicio': str(self.hora_inicio) if self.hora_inicio else None,
+            'hora_fin': str(self.hora_fin) if self.hora_fin else None
+        }
