49d19184756260800d1ef9df237591675fa2dca3
ariasgenesis <ariasgenesis2005@gmail.com>
2026-03-27 08:21:27 -0500

    Refactor app.py to simplify route loading

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
+# app.py
 from flask import Flask
 from flask_mysqldb import MySQL
-from routes import cargarRutas
+from routes import cargarRuta
 from config import Config
-from rutas.item_rutas import item_bp
 
 app = Flask(__name__)
-
 app.config.from_object(Config)
 
 mysql = MySQL(app)
 app.mysql = mysql
-
-app.register_blueprint(item_bp)
-
-cargarRutas(app)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=4000, host='0.0.0.0')
+cargarRuta(app)
+app.run(debug=True, port=4000, host='0.0.0.0')
