49c8b6ec15b439ec8f632ef8418735c2946799b1
rebecaiglesias43-bien <rebecaiglesias43@gmail.com>
2026-04-02 12:03:47 -0500

    Cambio de rutas al plural ("S" al final)

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,10 +3,12 @@ from .cita import cita_bp
 from .detalle_cita import detalle_cita_bp
 from .factura import factura_bp
 from .proveedor import proveedor_bp
+from .servicio import servicio_bp
 
 def cargarRutas(app):
-    app.register_blueprint(cliente_bp, url_prefix='/cliente')
-    app.register_blueprint(cita_bp, url_prefix='/cita')
-    app.register_blueprint(detalle_cita_bp, url_prefix='/detalle_cita')
-    app.register_blueprint(factura_bp, url_prefix='/factura')
-    app.register_blueprint(proveedor_bp, url_prefix='/proveedor')
+    app.register_blueprint(cliente_bp, url_prefix='/clientes')        # plural
+    app.register_blueprint(cita_bp, url_prefix='/citas')              # plural
+    app.register_blueprint(detalle_cita_bp, url_prefix='/detalle-citas')  # plural
+    app.register_blueprint(factura_bp, url_prefix='/facturas')        # plural
+    app.register_blueprint(proveedor_bp, url_prefix='/proveedores')   # plural
+    app.register_blueprint(servicio_bp, url_prefix='/servicios')      # plural
