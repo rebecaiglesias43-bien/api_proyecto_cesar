cadac34b500956aca42f2209105cd3842f162310
rebecaiglesias43-bien <rebecaiglesias43@gmail.com>
2026-04-15 15:41:51 -0500

    Add update and delete routes for citas

--- routes/citas.py
+++ routes/citas.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from controllers.citas_controllers import (
-    cntlistado_citas, cntobtener_cita, cntcrear_cita
+    cntlistado_citas, cntobtener_cita, cntcrear_cita,
+    cntactualizar_cita, cnteliminar_cita
 )
 
 cita_bp = Blueprint('citas', __name__)
@@ -16,3 +17,11 @@ def obtener(id_cita):
 @cita_bp.route('/', methods=['POST'])
 def crear():
     return cntcrear_cita()
+
+@cita_bp.route('/<int:id_cita>', methods=['PUT'])
+def actualizar(id_cita):
+    return cntactualizar_cita(id_cita)
+
+@cita_bp.route('/<int:id_cita>', methods=['DELETE'])
+def eliminar(id_cita):
+    return cnteliminar_cita(id_cita)
