5f72b1ee633fdaf6d552198e136bad6d125a995b
rebecaiglesias43-bien <rebecaiglesias43@gmail.com>
2026-04-15 15:34:13 -0500

    Add CRUD methods for ServicioProductoModel

--- models/servicios_productos_model.py
+++ models/servicios_productos_model.py
@@ -25,3 +25,36 @@ class ServicioProductoModel:
         for d in datos:
             resultado.append(ServicioProductoModel(d[0], d[1], d[2], d[3]).to_dict())
         return resultado
+
+    @staticmethod
+    def obtener_por_id(id_relacion):
+        cursor = current_app.mysql.connection.cursor()
+        cursor.execute("SELECT sep_id, sep_servicio_id, sep_producto_id, sep_cantidad FROM servicios_productos WHERE sep_id = %s", (id_relacion,))
+        d = cursor.fetchone()
+        cursor.close()
+        if d:
+            return ServicioProductoModel(d[0], d[1], d[2], d[3]).to_dict()
+        return None
+
+    @staticmethod
+    def actualizar(id_relacion, id_servicio, id_producto, cantidad):
+        cursor = current_app.mysql.connection.cursor()
+        cursor.execute(
+            "UPDATE servicios_productos SET sep_servicio_id = %s, sep_producto_id = %s, sep_cantidad = %s WHERE sep_id = %s",
+            (id_servicio, id_producto, cantidad, id_relacion)
+        )
+        current_app.mysql.connection.commit()
+        filas = cursor.rowcount
+        cursor.close()
+        if filas == 0:
+            return None
+        return ServicioProductoModel.obtener_por_id(id_relacion)
+
+    @staticmethod
+    def eliminar(id_relacion):
+        cursor = current_app.mysql.connection.cursor()
+        cursor.execute("DELETE FROM servicios_productos WHERE sep_id = %s", (id_relacion,))
+        current_app.mysql.connection.commit()
+        filas = cursor.rowcount
+        cursor.close()
+        return filas > 0
