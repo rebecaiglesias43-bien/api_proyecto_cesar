2469871e11a3982acb42162ad3200bec16bd26e5
rebecaiglesias43-bien <rebecaiglesias43@gmail.com>
2026-04-15 15:38:08 -0500

    Add methods to obtener, actualizar, and eliminar servicios

--- services/servicios_productos_services.py
+++ services/servicios_productos_services.py
@@ -6,3 +6,12 @@ class ServicioProductoService:
     
     def listar_todos(self):
         return ServicioProductoModel.listar_todos()
+
+    def obtener_por_id(self, id_relacion):
+        return ServicioProductoModel.obtener_por_id(id_relacion)
+
+    def actualizar(self, id_relacion, id_servicio, id_producto, cantidad):
+        return ServicioProductoModel.actualizar(id_relacion, id_servicio, id_producto, cantidad)
+
+    def eliminar(self, id_relacion):
+        return ServicioProductoModel.eliminar(id_relacion)
