c88bb2382cfd00f21923e6df7c6f4a3bbfcd016a
rebecaiglesias43-bien <rebecaiglesias43@gmail.com>
2026-04-15 15:45:44 -0500

    Add methods to manage DetalleCitaModel

--- services/detalles_citas_services.py
+++ services/detalles_citas_services.py
@@ -9,6 +9,15 @@ class DetalleCitaService:
     
     def listar_por_cita(self, id_cita):
         return DetalleCitaModel.listar_por_cita(id_cita)
-    
+
+    def obtener_por_id(self, id_detalle):
+        return DetalleCitaModel.obtener_por_id(id_detalle)
+
     def crear(self, id_cita, id_servicio, precio):
         return DetalleCitaModel.crear(id_cita, id_servicio, precio)
+
+    def actualizar(self, id_detalle, id_cita, id_servicio, precio):
+        return DetalleCitaModel.actualizar(id_detalle, id_cita, id_servicio, precio)
+
+    def eliminar(self, id_detalle):
+        return DetalleCitaModel.eliminar(id_detalle)
