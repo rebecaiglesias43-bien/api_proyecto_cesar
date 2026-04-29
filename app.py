0c7d0caa6cbea3b3e58049b05e9d7965f6e3eb82
Camila Cano <c45586573@gmail.com>
2026-04-29 13:47:22 -0500

    Agrego JWT, CORS y paginación

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 # app.py
 from flask import Flask
 from flask_mysqldb import MySQL
+
+import os
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
 from routes import cargarRutas  # Cambiado: cargarRuta → cargarRutas
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY') or 'change-me'
+CORS(app)
+JWTManager(app)
 
 mysql = MySQL(app)
 app.mysql = mysql
